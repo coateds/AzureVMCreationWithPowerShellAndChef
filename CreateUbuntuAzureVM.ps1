ee292eb31f0f58557542d144beea096e010db061
Dave Coate <coateds@outlook.com>
2018-02-27 08:09:33 -0800

    first version of Ubuntu script

--- CreateUbuntuAzureVM.ps1
+++ CreateUbuntuAzureVM.ps1
@@ -27,35 +27,15 @@ Function New-MyAzureVM ($loc, $name, $resGroup, $clientcred)
   $nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $resGroup -Location $loc -Name psLabNSG -SecurityRules $nsgRuleSSH,$nsgRuleWeb,$nsgRuleRdp
   
   # Create a virtual network card and associate it with the public IP address and NSG (appears in the portal after this command)
-  # $nic = New-AzureRmNetworkInterface -Name psLabNIC -ResourceGroupName $resGroup -Location $loc -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
-  
-  # Debug: No net security group
-  $nic = New-AzureRmNetworkInterface -Name psLabNIC -ResourceGroupName $resGroup -Location $loc -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id
+  $nic = New-AzureRmNetworkInterface -Name psLabNIC -ResourceGroupName $resGroup -Location $loc -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
   
   # Create a virtual machine configuration -Linux
-  # $vmConfig = New-AzureRmVMConfig -VMName $name -VMSize Standard_A1 | 
-  #   Set-AzureRmVMOperatingSystem -Linux -ComputerName $name -Credential $clientcred | 
-  #   Set-AzureRmVMSourceImage -PublisherName Canonical -Offer UbuntuServer -Skus 16.04-LTS -Version latest | 
-  #   Set-AzureRmVMOSDisk -Name psLabOSDisk -DiskSizeInGB 128 -CreateOption FromImage -Caching ReadWrite -StorageAccountType StandardLRS | 
-  #   Add-AzureRmVMNetworkInterface -Id $nic.Id
-
-  # Create a virtual machine configuration -Windows
-  # Renames and sets password for built-in admin account to $clientcred
   $vmConfig = New-AzureRmVMConfig -VMName $name -VMSize Standard_A1 | 
-    Set-AzureRmVMOperatingSystem -Windows -ComputerName $name -Credential $clientcred | 
-    Set-AzureRmVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2016-Datacenter -Version latest | 
+    Set-AzureRmVMOperatingSystem -Linux -ComputerName $name -Credential $clientcred | 
+    Set-AzureRmVMSourceImage -PublisherName Canonical -Offer UbuntuServer -Skus 16.04-LTS -Version latest | 
     Set-AzureRmVMOSDisk -Name psLabOSDisk -DiskSizeInGB 128 -CreateOption FromImage -Caching ReadWrite -StorageAccountType StandardLRS | 
     Add-AzureRmVMNetworkInterface -Id $nic.Id
 
-  # image_urn: MicrosoftWindowsServer:WindowsServer:2012-R2-Datacenter:latest
-  
-  # "offer": "WindowsServer",
-  # "publisher": "MicrosoftWindowsServer",
-  # "sku": "2016-Datacenter",
-  # "urn": "MicrosoftWindowsServer:WindowsServer:2016-Datacenter:latest",
-  # "urnAlias": "Win2016Datacenter",
-  # "version": "latest"
-  
   # Create the virtual machine
   # and the OSDisk
   # and the Storage Account
@@ -70,14 +50,13 @@ Function New-MyAzureVM ($loc, $name, $resGroup, $clientcred)
 # Set a variable to be your resource group's name
 # $location = "westus"
 # $vmName = "psLabVM"
-$resourceGroup = "psResourceGroup"
+$resourceGroup = "psUbuntuResourceGroup"
 
 # Create login credentials for your VM
 $securePassword = ConvertTo-SecureString 'H0rnyBunny' -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("coateds", $securePassword)
 
-New-MyAzureVM "westus" "psLabVM" $resourceGroup $cred
-
+New-MyAzureVM "westus" "psLabUbuntuVM" $resourceGroup $cred
 
 # These commands cannot be run inside the PE ISE. Go to ChefDK PS window
 # Test Connection:  ssh coateds@138.91.246.158
@@ -92,22 +71,7 @@ New-MyAzureVM "westus" "psLabVM" $resourceGroup $cred
 # knife bootstrap [public ip] --ssh-user [user] --ssh-password [password] --sudo --node-name node1-ubuntu --run-list 'recipe[ubuntu-installation-recipes::server-info-web]' --json-attributes '{"cloud": {"Public_ip": "[public ip]"}}'
 # knife bootstrap '104.42.70.234' --ssh-user 'coateds' --ssh-password 'H0rnyBunny' --sudo --node-name node1-ubuntu --run-list 'recipe[ubuntu-installation-recipes::server-info-web]' --json-attributes '{"cloud": {"Public_ip": "pslabvm"}}'
 
-# knife bootstrap '104.42.70.92' --ssh-user 'coateds' --ssh-password 'H0rnyBunny' --sudo --node-name node1-ubuntu --run-list 'role[ubuntuweb]' --json-attributes '{"cloud": {"Public_ip": "pslabvm"}}'
-
-# BootStrap a Windows VM
-# knife bootstrap windows winrm '192.168.0.206' -r 'role[my-basic-role]' -x administrator -P 'xxxxxxxx' -N 'Server3x'
-# Must open port 5985??
-# Must run winrm quickconfig
-# knife bootstrap windows winrm '13.64.148.123' -r 'role[my-basic-role]' -x coateds -P 'H0rnyBunny' -N 'node1-windows' --json-attributes '{"cloud": {"Public_ip": "pslabvm"}}'
-
-# PS C:\chef> gem install knife-windows
-# Fetching: knife-windows-1.9.0.gem (100%)
-# WARNING:  You don't have c:\users\administrator.coatelab\appdata\local\chefdk\gem\ruby\2.4.0\bin in your PATH,
-#           gem executables will not run.
-# Successfully installed knife-windows-1.9.0
-# Parsing documentation for knife-windows-1.9.0
-# Installing ri documentation for knife-windows-1.9.0
-# Done installing documentation for knife-windows after 1 seconds
+# knife bootstrap '40.78.103.239' --ssh-user 'coateds' --ssh-password 'H0rnyBunny' --sudo --node-name node1-ubuntu --run-list 'role[ubuntuweb]' --json-attributes '{"cloud": {"Public_ip": "pslabvm"}}'
 
 # Detritus
 # Get your resource group's name
---
+++ README.md
@@ -0,0 +1,3 @@
+# Azure VM Creation with PowerShell and Chef
+
+This repository contains PowerShell scripts for building Ubuntu and Windows Azure VMs. Then there is a command for each to bootstrap these VMs into a Chef server/organization.
